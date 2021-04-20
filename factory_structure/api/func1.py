c450a9110f47ee43bf9c2feb35499cab09775542
wilsonsujames <43058541+wilsonsujames@users.noreply.github.com>
2021-04-20 10:17:37 +0800

    Update func1.py

--- factory_structure/api/func1.py
+++ factory_structure/api/func1.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, Blueprint,jsonify,current_app
+from flask_mail import Mail, Message
 
 func1_blueprint = Blueprint('func1_blueprint', __name__)
 
 
 @func1_blueprint.route('/')
 def index():
+    msg = Message('Hello', sender = '*****@gmail.com', recipients = [ '*****@gmail.com'])
+    msg.body = 'hello hello hello'
+    with current_app.app_context():
+        mail = Mail()
+        mail.send(msg)
+    
+    
     return 'Index hello.'
     
